58a682c38ba23a0d96c9fece1083429931e7b5dc
Zunlin Zhang <larryzzl@gmail.com>
2021-12-21 18:13:44 +0800

    init

---
+++ BuildTest/BuildTest/BuildTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTestApp.swift
+//  BuildTest
+//
+//  Created by Zunlin Zhang on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BuildTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
